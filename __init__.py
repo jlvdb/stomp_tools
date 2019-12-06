0aa9c57eee8c40c25df3d0c3055308af3ca80c10
Jan Luca van den Busch <jlvdb@astro.rub.de>
2019-12-06 15:07:14 +0100

    fixed a bug that caused pointing maps to be empty

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .tools import (
     measure_region_area, regionize_data, generate_randoms,
-    adapt_mask, apply_mask, stdout_redirected)
+    adapt_mask, apply_mask, stdout_redirected, stderr_redirected)
